30ff9df8f03ed4735d89d0338fa3b3eba362fb62
kamron-dev <k.kamron@yadnex.ru>
2023-06-06 18:10:57 +0500

    created a working delete button that deletes the book from the array and updates the display

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bookNameInput = document.querySelector("#bookName");
 const authorNameInput = document.querySelector("#authorName");
 const numPagesInput = document.querySelector("#numPages");
 const readOrNot = document.querySelector("#readOrNot");
+const deleteBookBtn = document.querySelector(".deleteBookBtn");
 
 // Initializing display variable
 
@@ -23,30 +24,47 @@ function Book(title, author, numPages, readOrNot) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+
 }
 
 
 function displayMyLibrary() {
-  myLibrary.forEach(book => {
-    let newItemDiv = document.createElement("div");
+  myLibrary.forEach((book, index) => {
+    // Creating book divs
+    const newItemDiv = document.createElement("div");
     newItemDiv.classList.add("bookCard");
     newItemDiv.innerHTML = book.info();
     display.appendChild(newItemDiv);
 
-  })
+    //creating delete buttons
+    const deleteButton = document.createElement("button")
+    deleteButton.classList.add("deleteBookBtn");
+    deleteButton.dataset.indexNumber = index;
+    deleteButton.textContent = "Delete";
+    newItemDiv.appendChild(deleteButton);
 
+    //adding ability to delete books
+
+    deleteButton.addEventListener("click", () => {
+      myLibrary.splice(index, 1);
+      display.innerHTML = null;
+      displayMyLibrary();
+    })
+
+  })
   
 }
+
 
 submitButton.addEventListener("click", () => {
   let newBook = new Book(bookNameInput.value, authorNameInput.value, Number(numPagesInput.value), readOrNot.value);
   addBookToLibrary(newBook);
-  console.table(myLibrary);
+  // console.table(myLibrary);
   display.innerHTML = null;
-  displayMyLibrary()
-  
-  
+  displayMyLibrary();
 })
+
+
 
 
 
@@ -58,9 +76,6 @@ const survived2 = new Book("Survived2", "Jon Jones", 440, "read");
 myLibrary.push(stalker, stalker2, survived, survived2);
 
 displayMyLibrary();
-
-
-
 
 
 
